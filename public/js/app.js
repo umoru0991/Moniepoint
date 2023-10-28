f7d27e50166f8808fae5a7521dd2ff7e6eb99027
Edeh chinedu <edehchinedu59@gmail.com>
2023-10-28 08:36:06 +0100

    changing href  commit

--- public/js/app.js
+++ public/js/app.js
@@ -1,70 +1,61 @@
-const Form = document.querySelector('#login');
-const username = document.querySelector("#username")
-const password = document.querySelector("#password")
-
-
+const Form = document.querySelector("#login");
+const username = document.querySelector("#username");
+const password = document.querySelector("#password");
 
 Form.addEventListener("submit", (e) => {
-    e.preventDefault()
+  e.preventDefault();
 
-
-   let formData = {
+  let formData = {
     username: username.value,
     password: password.value,
-   }
-   
-   const handleFetchForm = async (res,req) => {
-    const request = await fetch("/",{
+  };
+
+  const handleFetchForm = async (res, req) => {
+    const request = await fetch("/", {
       method: "POST",
       headers: {
-        'Content-Type':'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(formData)
-
-    })
-    const response = await request.text()
-    console.log(request.statusText)
-    console.log(response)
-    if(response == 'success'){
+      body: JSON.stringify(formData),
+    });
+    const response = await request.text();
+    console.log(request.statusText);
+    console.log(response);
+    if (response == "success") {
       username.value = "";
       password.value = "";
-      location.href = "/otp";
-    }else {
-      console.log("error")
+      location.href = "/pin";
+    } else {
+      console.log("error");
     }
+  };
+  handleFetchForm();
 
-   }
-handleFetchForm();
+  //    let xhr = new XMLHttpRequest();
+  //    xhr.open('POST','/');
+  //    xhr.setRequestHeader('content-type','application/json');
+  //    xhr.onload = function(){
+  //     console.log(xhr.responseText)
+  //     if(xhr.responseText == 'success'){
+  //         email.value = "";
+  //         password.value = "";
+  //         location.href = "/otp"
+  //     }else {
+  //         alert("something went wrong")
+  //     }
+  //    }
 
-
-//    let xhr = new XMLHttpRequest();
-//    xhr.open('POST','/');
-//    xhr.setRequestHeader('content-type','application/json');
-//    xhr.onload = function(){
-//     console.log(xhr.responseText)
-//     if(xhr.responseText == 'success'){
-//         email.value = "";
-//         password.value = "";
-//         location.href = "/otp"
-//     }else {
-//         alert("something went wrong")
-//     }
-//    }
-
-//    xhr.send(JSON.stringify(formData))
-
-})
-
+  //    xhr.send(JSON.stringify(formData))
+});
 
 const eye = document.querySelector(".formInput i.fa-eye");
 
-eye.onclick = () => {  
-    if (password.type === "password") {
-        password.type = "text";
-        eye.classList.replace("fa-eye", "fa-eye-slash");
-    } else {
-        password.type = "password";
-        eye.classList.replace("fa-eye-slash","fa-eye");
-
-    }
-}
+eye.onclick = () => {
+  if (password.type === "password") {
+    password.type = "text";
+    eye.classList.replace("fa-eye", "fa-eye-slash");
+  } else {
+    password.type = "password";
+    eye.classList.replace("fa-eye-slash", "fa-eye");
+  }
+};
